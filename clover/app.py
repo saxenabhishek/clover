4176f4a18c76de88e74ba0e03e2201525887ed6b
saxenabhishek <as7122000@gmail.com>
2021-06-12 04:23:13 +0530

    🩹 Cors fixes

--- clover/app.py
+++ clover/app.py
@@ -1,10 +1,27 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
+from clover import book_router
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:3000",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(book_router, prefix="/api/books", tags=["books"])
+
+
 @app.get("/apis")
 async def f():
-    return "404 idhaar nahi hai"
+    return "404 idhaar nah hai"
 
-app.mount("/", StaticFiles(directory="./client/out",html = True), name="static")
+
+# app.mount("/", StaticFiles(directory="./client/out", html=True), name="static")
