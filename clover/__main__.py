6602a9aee6fbe5ce8474bc9e2c8e745ee581b01e
saxenabhishek <as7122000@gmail.com>
2021-06-12 04:22:30 +0530

    🚑 Reload only for py files

--- clover/__main__.py
+++ clover/__main__.py
@@ -1,4 +1,4 @@
 import uvicorn
 from clover import app
 
-uvicorn.run("clover:app", log_level="info", reload=True)
+uvicorn.run("clover:app", log_level="info", reload=True, reload_dirs=["./clover"])
