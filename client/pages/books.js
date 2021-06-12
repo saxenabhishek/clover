c49aa3d74a9a2da3f49a7bb765f5ce046a4f3693
saxenabhishek <as7122000@gmail.com>
2021-06-12 12:56:21 +0530

    ✨ Page stopers

--- client/pages/books.js
+++ client/pages/books.js
@@ -1,22 +1,35 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import api from "../components/api";
 import A_book from "../components/A_book";
 
 export default function books() {
   let [books, setBooks] = useState([]);
-  let [page, setPage] = useState(1);
+  let [page, setPage] = useState(400);
+  let [next, hasnext] = useState(false);
+  let [loading, setLoading] = useState(true);
   useEffect(() => {
+    setLoading(true)
     api
       .get("/books", {
         params: {
           page: page,
         },
       })
-      .then((data) => setBooks(data.data["message"]));
+      .then((data) => {
+        setLoading(false);
+        setBooks(data.data["message"]);
+      })
+      .catch((err) => {
+        console.log(err)
+        setLoading(false)
+        hasnext(true)
+        setPage(page - 1)
+
+      });
   }, [page]);
 
   function BookList(params) {
-    if (books.length === 0) {
+    if (loading) {
       return <p>Loading...</p>;
     }
     return books.map((b, key) => {
@@ -27,13 +40,33 @@ export default function books() {
       );
     });
   }
+  function isPrev() {
+    if (page == 1) return true;
+    return false;
+  }
+
   return (
     <div>
+      <h1>All Books</h1>
+      <p>Page : {page}</p>
       <BookList />
-      <button disabled={() => !!page} onClick={() => setPage(page - 1)}>
+      <button
+        disabled={isPrev()}
+        onClick={() => {
+          hasnext(false)
+          setPage(page - 1);
+        }}
+      >
         Prev
       </button>
-      <button onClick={() => setPage(page + 1)}>Next</button>
+      <button
+      disabled = {next}
+        onClick={() => {
+          setPage(page + 1);
+        }}
+      >
+        Next
+      </button>
     </div>
   );
 }
