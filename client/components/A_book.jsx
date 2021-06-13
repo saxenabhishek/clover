57dba9991f33be4310ba4ded04092d1070d3c30c
saxenabhishek <as7122000@gmail.com>
2021-06-14 01:35:47 +0530

    ✨ Indiv book compnents

--- client/components/A_book.jsx
+++ client/components/A_book.jsx
@@ -1,15 +1,64 @@
 import Link from "next/link";
+import router from "next/router";
+import { useEffect } from "react";
+import useBook from "./context/book";
 
 export default function A_book({ values }) {
+  const b = useBook();
+
+  function titleHelper(val) {
+    return val.length > 50 ? val.substring(0, 50) : val;
+  }
+  useEffect(() => {
+    if (typeof values.authors.split === "function") {
+      values.authors = values.authors.split("/");
+      if (values.authors.length > 50) {
+        values.authors = values.authors.substring(0, 50);
+      }
+    }
+  }, []);
+  function handelClick() {
+    console.log(b);
+    let t = b.tra;
+    t.book = values.isbn;
+    t.title = values.title;
+    b.setTra(t);
+    router.push("/issue");
+  }
   return (
-    <div>
-      <p>{values.title}</p>
-      <p>{values.average_rating}</p>
-      <Link href={"book/" + values.isbn}>
-        <button>details</button>
-      </Link>
-      <button>issue</button>
+    <div className="bg-custBlue3 mx-5 text-custWhite my-1 py-5 px-3 rounded-md">
+      <div className={"p-2 h-36 rounded-md rounded-r-xl "}>
+        <p className="text-xl font-semibold">{titleHelper(values.title)}</p>
+        <p className="text-lg font-light">{values.authors}</p>
+      </div>
+      <p>Rating : {values.average_rating}</p>
+      <NavBut vals={{ link: "/book/" + values.isbn, title: "Details" }} />
+      <NavButIss
+        vals={{ link: "/issue", title: "Issue", click: handelClick }}
+      />
       <br />
     </div>
   );
+
+  function NavBut({ vals }) {
+    return (
+      <Link href={vals.link}>
+        <button className="bg-custRed rounded-lg mx-1 p-1 text-custWhite w-15">
+          {vals.title}
+        </button>
+      </Link>
+    );
+  }
+  function NavButIss({ vals }) {
+    return (
+      <Link href={vals.link}>
+        <button
+          onClick={vals.click}
+          className="bg-custRed rounded-lg mx-1 p-1 text-custWhite w-15"
+        >
+          {vals.title}
+        </button>
+      </Link>
+    );
+  }
 }
