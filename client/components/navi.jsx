05cc022f89a9f97abfa595c32842cafecd65b1e0
saxenabhishek <as7122000@gmail.com>
2021-06-14 01:35:13 +0530

    ✨ Book select and search

---
+++ client/components/navi.jsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+
+export default function Navi(props) {
+  return (
+    <header className="w-full flex justify-between py-5 px-12 bg-custWhite text-custBlue3 sticky top-0 shadow-inner">
+      <div className="inline-flex">
+        <svg
+          stroke="currentColor"
+          fill="currentColor"
+          stroke-width="0"
+          viewBox="0 0 24 24"
+          height="2em"
+          width="2em"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M19,2.01H6c-1.206,0-3,0.799-3,3v3v6v3v2c0,2.201,1.794,3,3,3h15v-2H6.012C5.55,19.998,5,19.815,5,19.01 c0-0.101,0.009-0.191,0.024-0.273c0.112-0.575,0.583-0.717,0.987-0.727H20c0.018,0,0.031-0.009,0.049-0.01H21v-0.99V15V4.01 C21,2.907,20.103,2.01,19,2.01z M19,16.01H5v-2v-6v-3c0-0.806,0.55-0.988,1-1h7v7l2-1l2,1v-7h2V15V16.01z"></path>
+        </svg>
+        <h1 className="text-3xl font-medium">{props.val.text}</h1>
+      </div>
+      <div className="inline-flex">
+        <NavLinks val={{ link: "/books", text: "Books" }} />
+        <NavLinks val={{ link: "/", text: "Records" }} />
+        <NavLinks val={{ link: "/", text: "Checkouts" }} />
+      </div>
+    </header>
+  );
+
+  function NavLinks({ val }) {
+    return (
+      <Link href={val.link}>
+        <a className="hover:text-custRed px-2">{val.text}</a>
+      </Link>
+    );
+  }
+}
--- client/pages/books.js
+++ client/pages/books.js
@@ -1,6 +1,7 @@
 import { useEffect, useState, useRef } from "react";
 import api from "../components/api";
 import A_book from "../components/A_book";
+import BasicPage from "../components/BasicLayout";
 
 export default function books() {
   let [books, setBooks] = useState([]);
@@ -10,7 +11,7 @@ export default function books() {
   useEffect(() => {
     setLoading(true);
     api
-      .get("/books", {
+      .get("/books/", {
         params: {
           page: page,
         },
@@ -33,7 +34,7 @@ export default function books() {
     }
     return books.map((b, key) => {
       return (
-        <div key={key}>
+        <div className="w-1/4 my-4" key={key}>
           <A_book values={b} />
         </div>
       );
@@ -43,29 +44,56 @@ export default function books() {
     if (page == 1) return true;
     return false;
   }
+  function InBetPages() {
+    var buttArr = [];
+    for (var i = 1; i < 10; i++) {
+      buttArr.push(
+        <span
+          key={i}
+          className="rounded-full hover:bg-custRed text-xs p-1 px-2 mx-1 flex items-center justify-center  bg-custBlue3 "
+          onClick={(e) => {
+            setPage(parseInt(e.target.innerText));
+          }}
+        >
+          {i + page}
+        </span>
+      );
+    }
+    return <>{buttArr}</>;
+  }
+  const butt =
+    "bg-custRed mx-2 px-2 bg-custBlue3 bg-custRed shawdow-lg rounded-lg";
 
   return (
-    <div>
-      <h1>All Books</h1>
-      <p>Page : {page}</p>
-      <BookList />
-      <button
-        disabled={isPrev()}
-        onClick={() => {
-          hasnext(false);
-          setPage(page - 1);
-        }}
-      >
-        Prev
-      </button>
-      <button
-        disabled={next}
-        onClick={() => {
-          setPage(page + 1);
-        }}
-      >
-        Next
-      </button>
-    </div>
+    <BasicPage val ={{text:"All Books"}}>
+      <div className="flex flex-col mx-28 m-10 border-4 border-custWhite bg-custBlue2 text-custBlue p-4 px-8 rounded-3xl font-raleway shadow-xl">
+        <p className="font-normal text-2xl">Page : {page}</p>
+        <div className="flex flex-wrap">
+          <BookList />
+        </div>
+        <div className="mx-auto m-3 px-3 flex items-center text-custWhite">
+          <button
+            className={butt + " rounded-r-none"}
+            disabled={isPrev()}
+            onClick={() => {
+              hasnext(false);
+              setPage(page - 1);
+            }}
+          >
+            Prev
+          </button>
+          <InBetPages />
+          <button
+            className={butt + " rounded-l-none"}
+            disabled={next}
+            onClick={() => {
+              setPage(page + 1);
+            }}
+            >
+            Next
+          </button>
+        </div>
+      </div>
+  </BasicPage>
   );
 }
