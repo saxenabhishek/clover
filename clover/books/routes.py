c9848181023948b90aef261d7751d06d79c51eb9
saxenabhishek <as7122000@gmail.com>
2021-06-13 06:58:39 +0530

    ✨ issue and return books

--- clover/books/routes.py
+++ clover/books/routes.py
@@ -1,10 +1,21 @@
+from datetime import datetime
+from pydantic.main import BaseModel
+from starlette.status import HTTP_404_NOT_FOUND
+from bson import ObjectId
+from starlette.types import Message
+
 from clover.books.frappeAPI import AllBookPages, TisBook
+from clover.mongo.driver import CheckOutCON, recordsCON
+from clover.mongo.models import CheckOut, CheckOut_arr, Record
 from fastapi import APIRouter, HTTPException
 
 router = APIRouter()
 
 Allpages = AllBookPages()
 tis = TisBook()
+
+secRate = 0.2
+
 
 @router.get("/")
 async def get_books(page: int = 1):
@@ -21,3 +32,36 @@ async def get_this_book(isbn: str):
         raise HTTPException(status_code=404, detail="Item not found")
     return {"message": data}
 
+
+@router.post("/issue")
+async def give_this_book(rec: Record):
+    rec.when = datetime.now()
+    item = CheckOut_arr(userId=rec.userId, when=rec.when)
+    if await CheckOutCON.find_one({"isbn": rec.isbn}):
+        res = await CheckOutCON.update_one({"isbn": rec.isbn}, {"$inc": {"numb": 1}, "$push": {"issues": item.dict()}})
+    else:
+        entry = CheckOut(isbn=rec.isbn, issues=[item], numb=1)
+        res = await CheckOutCON.insert_one(entry.dict())
+    res2 = await recordsCON.insert_one(rec.dict())
+    return {"message": str(res2.inserted_id)}
+
+
+class returnDeets(BaseModel):
+    traId: str
+
+
+@router.put("/return")
+async def take_back_book(deets: returnDeets):
+    if d := await recordsCON.find_one({"_id": ObjectId(deets.traId)}):
+        d = Record(**d)
+        res = await CheckOutCON.update_one(
+            {"isbn": d.isbn}, {"$inc": {"numb": -1}, "$pull": {"issues": {"userId": d.userId}}}
+        )
+        delta = datetime.now() - d.when
+        cost = secRate * delta.total_seconds()
+        res2 = await recordsCON.update_one(
+            {"_id": ObjectId(deets.traId)}, {"$set": {"till": datetime.now(), "complete": True, "debt": cost}}
+        )
+        return {"message": {"cost": cost}}
+    else:
+        raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail="No transaction found")
--- clover/mongo/models.py
+++ clover/mongo/models.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 from pydantic import BaseModel
 from datetime import datetime
 
@@ -14,8 +14,8 @@ class User(BaseModel):
 
 
 class CheckOut_arr(BaseModel):
-    UserID: str
-    time: datetime
+    userId: str
+    when: datetime
 
 
 class CheckOut(BaseModel):
@@ -26,7 +26,20 @@ class CheckOut(BaseModel):
 
 class Record(BaseModel):
     isbn: str
-    userID: str
-    when: datetime
-    till: datetime
+    userId: str
+    when: Optional[datetime] = None
+    till: Optional[datetime] = None
     debt: int = 0
+    complete: bool = False
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "isbn": "0226731510",
+                "userId": "60c52cd30536c26314ddf528",
+                "when": "2021-06-12T23:52:27.351Z",
+                "till": "2021-06-12T23:52:27.351Z",
+                "debt": 0,
+                "complete": False,
+            }
+        }
