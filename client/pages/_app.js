5e4e090cb9cf867e8831242118b94b0cd1f60d23
saxenabhishek <as7122000@gmail.com>
2021-06-14 02:13:04 +0530

    🐛 fix Context Provider

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,11 @@
-import '../styles/globals.css'
-
+import "../styles/globals.css";
+import { Bookprovider } from "../components/context/book";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Bookprovider>
+      <Component {...pageProps} />
+    </Bookprovider>
+  );
 }
 
-export default MyApp
+export default MyApp;
