b8f348201f7c8f358be070e0d3d2970725db806c
saxenabhishek <as7122000@gmail.com>
2021-06-14 10:59:53 +0530

    🚑 us.map not found crash

--- client/pages/issue.js
+++ client/pages/issue.js
@@ -28,12 +28,16 @@ export default function issue(props) {
     if (us.length === 0) {
       return <p className="mx-auto text-xl font-light">No matching users</p>;
     }
+    if (typeof us.map !== "function") {
+      return <p>Returning book</p>;
+    }
     return us.map((i, k) => {
       function handelClick(e) {
         var T = b.tra;
         T.user = i._id;
         setUs(T);
         b.issueBook();
+        router.push("/checkouts");
       }
       return (
         <div key={k} className="flex justify-between">
@@ -43,7 +47,7 @@ export default function issue(props) {
           </div>
           <button
             onClick={handelClick}
-            className="rounded-md px-10 py-2 m-2 bg-custRed text-custWhite shadow-md"
+            className="rounded-md px-10 py-2 m-2 bg-custBlue2 hover:bg-custRed text-custWhite shadow-md"
           >
             Select
           </button>
@@ -68,7 +72,7 @@ export default function issue(props) {
   }
   return (
     <BasicPage val={{ text: "Issuing " + b.tra.title }}>
-      <div className="Container bg-custWhite mx-auto w-3/4 p-5 text-custBlue3 flex flex-col rounded-lg shadow-xl border-4 border-custRed">
+      <div className="Container bg-custWhite mx-auto w-3/4 p-5 text-custBlue3 flex flex-col rounded-lg shadow-xl border-4 border-custBlue2">
         <div className="m-3">
           <label className="text-lg ">Search </label>
           <input onChange={Changing} className="bg-custBlue rounded-md"></input>
