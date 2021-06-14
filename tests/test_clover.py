588aa6f1d1597d66e9f57dee6fef81d798b1547a
saxenabhishek <as7122000@gmail.com>
2021-06-15 02:58:15 +0530

    ✅  alive, duplication

--- tests/test_clover.py
+++ tests/test_clover.py
@@ -1,5 +1,24 @@
-from clover import __version__
+from fastapi.testclient import TestClient
+from starlette import responses
+from clover import app
+
+client = TestClient(app)
 
 
-def test_version():
-    assert __version__ == '0.1.0'
+def test_alive():
+    response = client.get("/api")
+    assert response.status_code == 200
+    assert response.json() == "Backend API is up"
+
+
+# def test_create_user():
+#     response = client.post("/api/users/make_user", json={"name": "TestClient", "passw": "SuperSecret"})
+#     print(response.json())
+#     assert response.status_code == 201
+#     assert response.json()["inserted_id"]
+
+
+def test_create_duplicate_user():
+    response = client.post("/api/users/make_user", json={"name": "TestClient", "passw": "SuperSecret"})
+    assert response.status_code == 400
+    assert response.json() == {"detail": "User Aleady exists"}
