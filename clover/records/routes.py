5e53f3df68c3924c944f4bf9d64a78861ce57b28
saxenabhishek <as7122000@gmail.com>
2021-06-14 11:02:59 +0530

    ✨ name title in records

--- clover/records/routes.py
+++ clover/records/routes.py
@@ -1,16 +1,20 @@
 from typing import List, Optional
 
 from bson import ObjectId
-from clover.mongo.driver import recordsCON
+import requests
+from clover.mongo.driver import recordsCON, userCON
 from clover.mongo.models import PyObjectId, Record
 from fastapi import APIRouter, HTTPException, status
 from pydantic import Field
 
 router = APIRouter()
+FrappeBase = "https://frappe.io/api/method/frappe-library"
 
 
 class recRes(Record):
     id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+    name: str
+    bookTitle: str
 
     class Config:
         allow_population_by_field_name = True
@@ -26,7 +30,13 @@ async def get_records(completed: Optional[bool] = None, page: int = 0):
         find_args["complete"] = completed
 
     L = recordsCON.find(find_args).sort("_id", 1).skip(page * 10)
-    res = [recRes(**i) for i in await L.to_list(length=10)]
+    res = []
+    for i in await L.to_list(length=10):
+        tempName = await userCON.find_one({"_id": ObjectId(i["userId"])})
+        tempName = tempName["name"]
+        tempTitle = requests.get(FrappeBase, {"isbn": i["isbn"]}).json()
+        tempTitle = tempTitle["message"][0]["title"]
+        res.append(recRes(**i, name=tempName, bookTitle=tempTitle))
     if len(res) == 0:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="This page doesn't exist")
     return res
